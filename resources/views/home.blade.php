1c34fb1f5a3676bad0201b9a8d7434a06b4661fb
Anthony Serino <tony@serino.me>
2016-04-21 14:57:50 -0400

    CineBound

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,89 +4,46 @@
 
   <div id="app">
 
-    <h1>Movies</h1>
-    <ul class="list-group">
-      <li class="list-group-item" v-for="movie in movies">
-        @{{ movie.title }}
-      </li>
-    </ul>
-
     <div class="form-group">
-      <label for="title">Search</label>
-      <input v-model="search" v-on:keyup.enter="searchMovies" class="form-control" />
+      <div class="input-group">
+        <input v-model="search" v-on:keyup.enter="searchMovies" class="form-control" placeholder="Find a Movie" />
+        <span class="input-group-btn">
+          <button v-on:click="searchMovies" class="btn btn-default" type="button">Search</button>
+        </span>
+      </div>
     </div>
 
-    <div v-if="results">
-      <ul class="list-group">
-        <li class="list-group-item" v-for="result in results">
-          <img src="@{{img_base + result.poster_path}}" />
-          @{{ result.title }} - @{{ result.release_date }}
-        </li>
-      </ul>
+    <div class="container" v-if="results">
+      <div class="media" v-for="result in results">
+        <div class="media-left">
+          <a href="#">
+            <img class="media-object" src="@{{img_base + result.poster_path}}" alt="">
+          </a>
+        </div>
+        <div class="media-body">
+          <h4 class="media-heading">@{{ result.title }}</h4>
+          @{{ result.release_date }} <br />
+          <button class="btn btn-primary" v-on:click="addMovie">Add Movie</button>
+        </div>
+      </div>
     </div>
 
-    <div class="form-group">
-      <label for="title">Title</label>
-      <input v-model="newMovie.title" class="form-control" />
-    </div>
-    <div class="form-group">
-      <label for="studio">Studio</label>
-      <select v-model="newMovie.studio" class="form-control">
-        <option v-for="studio in studios">@{{studio.name}}</option>
-      </select>
-    </div>
-    <div class="form-group">
-      <label for="release_date">Release Date</label>
-      <input type="date" v-model="newMovie.release_date" class="form-control" />
-    </div>
-    <div class="form-group">
-      <button class="btn btn-primary" v-on:click="addMovie">Add Movie</button>
-    </div>
-
-    <h1>Studios</h1>
-    <ul class="list-group">
-      <li class="list-group-item" v-for="studio in studios">
-        @{{ studio.name }}
-      </li>
-    </ul>
-    <div class="form-group">
-      <label for="title">Name</label>
-      <input v-model="newStudio.name" class="form-control" />
-    </div>
-    <div class="form-group">
-      <button class="btn btn-primary" v-on:click="addStudio">Add Studio</button>
-    </div>
-  </div>
-
-  <script type="text/javascript">
-  new Vue({
-    el: '#app',
-    data: {
-      newMovie: {},
-      movies: [],
-      newStudio: {},
-      studios: [{name:'Marvel'}],
-      search: '',
-      results: [],
-      img_base: 'http://image.tmdb.org/t/p/w184'
-    },
-    methods: {
-      searchMovies: function(){
-        var that = this;
-        $.post('/api/movies', {query: this.search}, function(data){
-          that.results = data.results;
-        }, 'JSON');
+    <script type="text/javascript">
+    new Vue({
+      el: '#app',
+      data: {
+        results: [],
+        img_base: 'http://image.tmdb.org/t/p/w184'
       },
-      addMovie: function(){
-        this.movies.push(this.newMovie);
-        this.newMovie = {};
-      },
-      addStudio: function(){
-        this.studios.push(this.newStudio);
-        this.newStudio = {};
+      methods: {
+        searchMovies: function(){
+          var that = this;
+          $.post('/api/movies', {query: this.search}, function(data){
+            that.results = data.results;
+          }, 'JSON');
+        }
       }
-    }
-  })
-  </script>
+    })
+    </script>
 
-@endsection
+  @endsection
