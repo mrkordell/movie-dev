3de5d2504b660a59204b165acb4da0412dbd9189
Anthony Serino <tony@serino.me>
2016-04-21 17:33:05 -0400

    Socialite login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use Socialite;
 use App\User;
 use Validator;
+use Auth;
+use Redirect;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
@@ -12,76 +14,102 @@ use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
 class AuthController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Registration & Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users, as well as the
-    | authentication of existing users. By default, this controller uses
-    | a simple trait to add these behaviors. Why don't you explore it?
-    |
-    */
+  /*
+  |--------------------------------------------------------------------------
+  | Registration & Login Controller
+  |--------------------------------------------------------------------------
+  |
+  | This controller handles the registration of new users, as well as the
+  | authentication of existing users. By default, this controller uses
+  | a simple trait to add these behaviors. Why don't you explore it?
+  |
+  */
 
-    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+  use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
-    /**
-     * Where to redirect users after login / registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/';
+  /**
+  * Where to redirect users after login / registration.
+  *
+  * @var string
+  */
+  protected $redirectTo = '/';
 
-    /**
-     * Create a new authentication controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
+  /**
+  * Create a new authentication controller instance.
+  *
+  * @return void
+  */
+  public function __construct()
+  {
+    $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
+  }
+
+  public function redirectToProvider($provider = 'google')
+  {
+    return Socialite::with($provider)->redirect();
+  }
+  public function handleProviderCallback($provider = 'google')
+  {
+
+    try {
+      $user = Socialite::with($provider)->user();
+    } catch (Exception $e) {
+      return Redirect::to('auth/' . $provider);
     }
 
-    public function redirectToProvider($provider = 'google')
-    {
-        return Socialite::with($provider)->redirect();
-    }
-    public function handleProviderCallback($provider = 'google')
-    {
-        $user = Socialite::with($provider)->user();
+    $authUser = $this->findOrCreateUser($user, $provider);
 
-        dd($user);
+    Auth::login($authUser, true);
 
-        // $user->token;
-    }
+    return Redirect::to('/');
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|min:6|confirmed',
-        ]);
+    // $user->token;
+  }
+
+  private function findOrCreateUser($user, $provider)
+  {
+    if ($authUser = User::where( $provider . '_id', $user->id)->first()) {
+      return $authUser;
     }
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
-    }
+    $provider = ($provider . '_id');
+
+    $u = new User;
+    $u->name = $user->name;
+    $u->email = $user->email;
+    $u->$provider = $user->id;
+    $u->avatar = $user->avatar;
+    $u->save();
+    return $u;
+  }
+
+  /**
+  * Get a validator for an incoming registration request.
+  *
+  * @param  array  $data
+  * @return \Illuminate\Contracts\Validation\Validator
+  */
+  protected function validator(array $data)
+  {
+    return Validator::make($data, [
+      'name' => 'required|max:255',
+      'email' => 'required|email|max:255|unique:users',
+      'password' => 'required|min:6|confirmed',
+    ]);
+  }
+
+  /**
+  * Create a new user instance after a valid registration.
+  *
+  * @param  array  $data
+  * @return User
+  */
+  protected function create(array $data)
+  {
+    return User::create([
+      'name' => $data['name'],
+      'email' => $data['email'],
+      'password' => bcrypt($data['password']),
+    ]);
+  }
 }
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,46 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>CineBound</title>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>CineBound</title>
 
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
-    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.21/vue.js"></script>
-  </head>
-  <body>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <h1 style="font-family: 'Montserrat', sans-serif;">CineBound</h1>
-        </div>
-        <div class="col-md-6">
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.21/vue.js"></script>
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <h1 style="font-family: 'Montserrat', sans-serif;">CineBound</h1>
+      </div>
+      <div class="col-md-6">
+        @if(Auth::check())
+          <div class="pull-right">
+            <img src="{{Auth::user()->avatar}}" /> {{Auth::user()->name}}
+          </div>
+        @else
+
           <a href="/auth/facebook"><img src="facebook_login.png" alt="Sign In with Facebook" class="pull-right" style="margin-top:20px;" /></a>
           <a href="/auth/google"><img src="/google_signin_buttons/web/1x/btn_google_signin_light_pressed_web.png" alt="Sign In with Google" class="pull-right" style="margin-top:20px;" /></a>
-        </div>
-        <div class="col-md-12">
-          @yield('content')
-        </div>
+        @endif
+      </div>
+      <div class="col-md-12">
+        @yield('content')
       </div>
     </div>
+  </div>
 
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-  </body>
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
 </html>
