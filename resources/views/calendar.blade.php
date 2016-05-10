f765802df3995782f82045af20ccc141d5c88e43
Anthony Serino <tony@serino.me>
2016-05-10 19:33:42 -0400

    formatting fix

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,5 +1,3 @@
 BEGIN:VCALENDAR{{"\r\n"}}VERSION:2.0{{"\r\n"}}PRODID:-//hacksw/handcal//NONSGML v1.0//EN{{"\r\n"}}CALSCALE:GREGORIAN
-@foreach($movies as $movie)
-{{"\r\n"}}BEGIN:VEVENT{{"\r\n"}}DTEND:{{date('Ymd\THis', strtotime($movie->release_date))}}{{"\r\n"}}UID:{{$movie->id}}{{"\r\n"}}DTSTAMP:{{date('Ymd\THis', strtotime($movie->release_date))}}{{"\r\n"}}SUMMARY:{{$movie->title}}{{"\r\n"}}URL;VALUE=URI:http://www.cinebound.com/movie/{{$movie->id}}{{"\r\n"}}DTSTART:{{date('Ymd\THis', strtotime($movie->release_date))}}{{"\r\n"}}END:VEVENT
-@endforeach
-{{"\r\n"}}END:VCALENDAR
+@foreach($movies as $movie){{"\r\n"}}BEGIN:VEVENT{{"\r\n"}}DTEND:{{date('Ymd\THis', strtotime($movie->release_date))}}{{"\r\n"}}UID:{{$movie->id}}{{"\r\n"}}DTSTAMP:{{date('Ymd\THis', strtotime($movie->release_date))}}{{"\r\n"}}SUMMARY:{{$movie->title}}{{"\r\n"}}URL;VALUE=URI:http://www.cinebound.com/movie/{{$movie->id}}{{"\r\n"}}DTSTART:{{date('Ymd\THis', strtotime($movie->release_date))}}{{"\r\n"}}END:VEVENT
+@endforeach{{"\r\n"}}END:VCALENDAR
