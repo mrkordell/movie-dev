b5f2d288a97a2add42b1edcbd197a156522ebe35
Anthony Serino <tony@serino.me>
2016-05-10 19:18:16 -0400

    cal format fixes

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,15 +1,15 @@
-BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:-//hacksw/handcal//NONSGML v1.0//EN
-CALSCALE:GREGORIAN
+<?php header('Content-type: text/calendar; charset=utf-8');?>BEGIN:VCALENDAR{{"\r\n"}}
+VERSION:2.0{{"\r\n"}}
+PRODID:-//hacksw/handcal//NONSGML v1.0//EN{{"\r\n"}}
+CALSCALE:GREGORIAN{{"\r\n"}}
 @foreach($movies as $movie)
-BEGIN:VEVENT
-DTEND:{{date('Ymd\THis', strtotime($movie->release_date))}}
-UID:{{$movie->id}}
-DTSTAMP:{{date('Ymd\THis', strtotime($movie->release_date))}}
-DESCRIPTION:{{$movie->title}}
-URL;VALUE=URI:http://www.cinebound.com/movie/{{$movie->id}}
-DTSTART:{{date('Ymd\THis', strtotime($movie->release_date))}}
-END:VEVENT
+BEGIN:VEVENT{{"\r\n"}}
+DTEND:{{date('Ymd\THis', strtotime($movie->release_date))}}{{"\r\n"}}
+UID:{{$movie->id}}{{"\r\n"}}
+DTSTAMP:{{date('Ymd\THis', strtotime($movie->release_date))}}{{"\r\n"}}
+DESCRIPTION:{{$movie->title}}{{"\r\n"}}
+URL;VALUE=URI:http://www.cinebound.com/movie/{{$movie->id}}{{"\r\n"}}
+DTSTART:{{date('Ymd\THis', strtotime($movie->release_date))}}{{"\r\n"}}
+END:VEVENT{{"\r\n"}}
 @endforeach
 END:VCALENDAR
