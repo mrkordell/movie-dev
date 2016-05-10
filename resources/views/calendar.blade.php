ee43418cc22385ed4e9e39a35f04a1b4c27aa66f
Anthony Serino <tony@serino.me>
2016-05-10 19:31:37 -0400

    change description to summary

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,5 +1,5 @@
 BEGIN:VCALENDAR{{"\r\n"}}VERSION:2.0{{"\r\n"}}PRODID:-//hacksw/handcal//NONSGML v1.0//EN{{"\r\n"}}CALSCALE:GREGORIAN
 @foreach($movies as $movie)
-{{"\r\n"}}BEGIN:VEVENT{{"\r\n"}}DTEND:{{date('Ymd\THis', strtotime($movie->release_date))}}{{"\r\n"}}UID:{{$movie->id}}{{"\r\n"}}DTSTAMP:{{date('Ymd\THis', strtotime($movie->release_date))}}{{"\r\n"}}DESCRIPTION:{{$movie->title}}{{"\r\n"}}URL;VALUE=URI:http://www.cinebound.com/movie/{{$movie->id}}{{"\r\n"}}DTSTART:{{date('Ymd\THis', strtotime($movie->release_date))}}{{"\r\n"}}END:VEVENT
+{{"\r\n"}}BEGIN:VEVENT{{"\r\n"}}DTEND:{{date('Ymd\THis', strtotime($movie->release_date))}}{{"\r\n"}}UID:{{$movie->id}}{{"\r\n"}}DTSTAMP:{{date('Ymd\THis', strtotime($movie->release_date))}}{{"\r\n"}}SUMMARY:{{$movie->title}}{{"\r\n"}}URL;VALUE=URI:http://www.cinebound.com/movie/{{$movie->id}}{{"\r\n"}}DTSTART:{{date('Ymd\THis', strtotime($movie->release_date))}}{{"\r\n"}}END:VEVENT
 @endforeach
 {{"\r\n"}}END:VCALENDAR
