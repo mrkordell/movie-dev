1865cde70aec726ee437b29526c9d6ca5077496a
Anthony Serino <tony@serino.me>
2016-05-10 19:11:04 -0400

    ical format fix

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -2,16 +2,14 @@ BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//hacksw/handcal//NONSGML v1.0//EN
 CALSCALE:GREGORIAN
-
 @foreach($movies as $movie)
-  BEGIN:VEVENT
-  DTEND:{{$movie->release_date}}
-  UID:{{$movie->id}}
-  DTSTAMP:{{$movie->release_date}}
-  DESCRIPTION:{{$movie->title}}
-  URL;VALUE=URI:http://www.cinebound.com/movie/{{$movie->id}}
-  DTSTART:{{$movie->release_date}}
-  END:VEVENT
+BEGIN:VEVENT
+DTEND:{{date('Ymd\THis', strtotime($movie->release_date))}}
+UID:{{$movie->id}}
+DTSTAMP:{{date('Ymd\THis', strtotime($movie->release_date))}}
+DESCRIPTION:{{$movie->title}}
+URL;VALUE=URI:http://www.cinebound.com/movie/{{$movie->id}}
+DTSTART:{{date('Ymd\THis', strtotime($movie->release_date))}}
+END:VEVENT
 @endforeach
-
 END:VCALENDAR
