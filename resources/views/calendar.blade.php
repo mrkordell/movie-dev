9165b3f8584a63002e4943ae93865242a53fae37
Anthony Serino <tony@serino.me>
2016-05-10 21:17:16 -0400

    all day event

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -6,7 +6,6 @@ CALSCALE:GREGORIAN
 BEGIN:VEVENT
 DTEND:{{date('Ymd\THis', strtotime($movie->release_date))}}
 UID:{{$movie->id}}
-DTSTAMP:{{date('Ymd\THis', strtotime($movie->release_date))}}
 SUMMARY:{{$movie->title}}
 URL;VALUE=URI:http://www.cinebound.com/movie/{{$movie->id}}
 DTSTART:{{date('Ymd\THis', strtotime($movie->release_date))}}
